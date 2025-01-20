e89ac8ebff46e7bcd4032d74468223e38b63d6b4
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-01-19 22:34:29 -0400

    Added NameSearch exercise

---
+++ Assignments/NameSearch/NameSearch/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace NameSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoyName = new System.Windows.Forms.TextBox();
+            this.txtGirlName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Type in a name and click SEARCH to see if it is one of the popular given to boys " +
+    "or girls from 2000 to 2009";
+            // 
+            // txtBoyName
+            // 
+            this.txtBoyName.Location = new System.Drawing.Point(142, 237);
+            this.txtBoyName.Name = "txtBoyName";
+            this.txtBoyName.Size = new System.Drawing.Size(156, 22);
+            this.txtBoyName.TabIndex = 1;
+            // 
+            // txtGirlName
+            // 
+            this.txtGirlName.Location = new System.Drawing.Point(436, 237);
+            this.txtGirlName.Name = "txtGirlName";
+            this.txtGirlName.Size = new System.Drawing.Size(156, 22);
+            this.txtGirlName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Boy";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(501, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Girl";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(291, 300);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(152, 50);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGirlName);
+            this.Controls.Add(this.txtBoyName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Name Search - Gabriel Rodrigues";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoyName;
+        private System.Windows.Forms.TextBox txtGirlName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Assignments/NameSearch/NameSearch/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NameSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> boysNames= new List<string>();
+
+        List<string> girlsNames = new List<string>();
+
+        StreamReader boysInputFile = File.OpenText("BoyNames.txt");
+
+        StreamReader girlsInputFile = File.OpenText("GirlNames.txt");
+
+        string boyName, girlName;
+        private void ReadBoysFile()
+        {
+            int count = 0;
+
+            while (!boysInputFile.EndOfStream) 
+            {
+                boysNames.Add(boysInputFile.ReadLine());
+                count++;
+            }
+            boysInputFile.Close();
+        }
+
+        private void ReadGirlsFile() 
+        {
+            int count = 0;
+
+            while (!girlsInputFile.EndOfStream) 
+            {
+                girlsNames.Add(girlsInputFile.ReadLine());
+                count++;
+            }
+            girlsInputFile.Close();
+        }
+
+        private string getBoyName()
+        {
+            boyName = txtBoyName.Text;
+            return boyName;
+        }
+
+        private string getGirlName() 
+        {
+            girlName = txtGirlName.Text;
+            return girlName;
+        }
+
+        private int Search(List<string> inputList, string name)
+        {
+            bool found = false;
+            int position = -100;
+            int index = 0;
+
+            while (!found && (index < inputList.Count))
+            {
+                if (inputList[index] == name)
+                {
+                    position = index;
+                    found = true;
+                }
+                index++;
+            }
+            return position;
+        }
+
+        private void CompareBoyName(string boyName)
+        {
+            if (Search(boysNames, boyName) != -100)
+            {
+                MessageBox.Show("The name " + boyName + " is among the most popular for boys.");
+            }
+            else
+            {
+                MessageBox.Show("The name " + boyName + " is not among the most popular for boys.");
+            }
+        }
+
+        private void CompareGirlName(string girlName)
+        {
+            if (Search(girlsNames, girlName) != -100)
+            {
+                MessageBox.Show("The name " + girlName + " is among the most popular for girls.");
+            }
+            else
+            {
+                MessageBox.Show("The name " + girlName + " is not among the most popular for girls.");
+            }
+        }
+
+        private void CompareBothNames(string boyName, string girlName)
+        {
+            if ((Search(boysNames, boyName) != -100) && (Search(girlsNames, girlName) != -100))
+            {
+                MessageBox.Show("The name '" + boyName + "' is among the most popular for boys.\n\n" +
+                    "The name '" + girlName + "' is among the most popular for girls.");
+            }
+            else if ((Search(boysNames, boyName) != -100) && (Search(girlsNames, girlName) == -100))
+            {
+                MessageBox.Show("The name '" + boyName + "' is among the most popular for boys.\n\n" +
+                    "The name '" + girlName + "' is not among the most popular for girls.");
+            }
+            else if ((Search(boysNames, boyName) == -100) && (Search(girlsNames, girlName) != -100))
+            {
+                MessageBox.Show("The name '" + boyName + "' is not among the most popular for boys.\n\n" +
+                    "The name '" + girlName + "' is among the most popular for girls.");
+            }
+            else
+            {
+                MessageBox.Show("The name '" + boyName + "' is not among the most popular for boys.\n\n" +
+                    "The name '" + girlName + "' is not among the most popular for girls.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReadBoysFile();
+            ReadGirlsFile();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            getBoyName();
+            getGirlName();
+
+            if (boyName != "" && girlName != "")
+            {
+                CompareBothNames(boyName, girlName);
+                txtBoyName.Focus();
+                txtBoyName.Text = string.Empty;
+                txtGirlName.Text = string.Empty;
+            }
+            else if (boyName != "" && girlName == "")
+            {
+                CompareBoyName(boyName);
+                txtBoyName.Focus();
+                txtBoyName.Text = string.Empty;
+                txtGirlName.Text = string.Empty;
+            }
+            else if (girlName != "" && boyName == "")
+            {
+                CompareGirlName(girlName);
+                txtGirlName.Focus();
+                txtBoyName.Text = string.Empty;
+                txtGirlName.Text = string.Empty;
+            }
+        }
+    }
+}
