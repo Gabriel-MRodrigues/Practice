9115c0e7991847cce799604510dcb5fca5149796
Gabriel Rodrigues <gabrielmendesrodriguest@hotmail.com>
2025-02-07 15:29:20 -0400

    Added MergeSort algorithm

---
+++ Algorithms/mergeSort.js
@@ -0,0 +1,31 @@
+function MergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const midIndex = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, midIndex);
+  const rightArr = arr.slice(midIndex, arr.length);
+
+  return Merge(MergeSort(leftArr), MergeSort(rightArr));
+}
+
+function Merge(leftArr, rightArr) {
+  let resultArr = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    if (leftArr[leftIndex] < rightArr[rightIndex]) {
+      resultArr.push(leftArr[leftIndex]);
+      leftIndex++;
+    } else {
+      //meaning: rightArr[rightIndex] < leftArr[leftIndex]
+      resultArr.push(rightArr[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return resultArr.concat(
+    leftArr.slice(leftIndex).concat(rightArr.slice(rightIndex))
+  );
+}
