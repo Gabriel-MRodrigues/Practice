b1e2b5be4f707e2a9dcdddc94191c810ea3a0ebe
Gabriel-MRodrigues <gabrielmendesrodriguest@hotmail.com>
2025-04-22 15:37:10 -0300

    adding useEffect functionality

--- Practicing/my-first-react-app/src/App.jsx
+++ Practicing/my-first-react-app/src/App.jsx
@@ -3,9 +3,21 @@ import { useState } from 'react';
 
 const Card = ({ title }) => {
   const [hasLiked, setHasLiked] = useState(false);
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`);
+  }, [hasLiked]);
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div
+      className="card"
+      onClick={() => setCount((pervState) => pervState + 1)}
+    >
+      <h2>
+        {title} <br /> {count || null}
+      </h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '✨' : '⭐'}
       </button>
